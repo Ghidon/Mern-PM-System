a7c0c8a4963723972282dc482ee502bc47291817
Ghidon Fadlun <ghidon@hotmail.com>
2021-11-11 14:10:28 +0200

    fixed task layout on two columns

--- client/src/components/Projects/Project/Tasks/Task/Task.js
+++ client/src/components/Projects/Project/Tasks/Task/Task.js
@@ -241,241 +241,252 @@ const Task = () => {
                 )}
               </div>
             </form>
-          </div>
-          {taskData.attachedFiles.length ? (
-            <div className="col-12 col-md-5 mb-3">
-              <span>Attached files</span>
-              {taskData.attachedFiles.map((file) => (
-                <div
-                  className="card d-flex flex-row p-1 mb-1 shadow"
-                  key={file.base64}
+            <div className="d-flex flex-wrap justify-content-between">
+              <div className="d-flex mb-3">
+                <button
+                  className="btn btn-secondary shadow"
+                  onClick={() => {
+                    document.getElementById("taskForm").disabled = false;
+                    document
+                      .getElementById("saveButton")
+                      .classList.remove("disabled");
+                  }}
                 >
-                  <span>{file.name}</span>
-                  <span className="ms-auto">
-                    <a download={file.name} href={file.base64}>
+                  Edit
+                </button>
+
+                <button
+                  type="submit"
+                  id="saveButton"
+                  onClick={() => {
+                    handleSubmit();
+                  }}
+                  className="btn btn-primary disabled shadow mx-3"
+                >
+                  Save
+                </button>
+                <div>
+                  <div className="input-group shadow">
+                    <label
+                      className="input-group-text"
+                      htmlFor="inputGroupSelect01"
+                    >
+                      status
+                    </label>
+                    <select
+                      className="form-select"
+                      id="inputGroupSelect01"
+                      onChange={(e) => setNewStatus(e)}
+                    >
+                      <option value="default" defaultValue>
+                        {taskData.status}
+                      </option>
+                      {taskData.status !== "To do" && (
+                        <option value="To do">To do</option>
+                      )}
+                      {taskData.status !== "In progress" && (
+                        <option value="In progress">In progress</option>
+                      )}
+                      {taskData.status !== "Blocked" && (
+                        <option value="Blocked">Blocked</option>
+                      )}
+                      {taskData.status !== "Done" && (
+                        <option value="Done">Done</option>
+                      )}
+                    </select>
+                  </div>
+                </div>
+              </div>
+              <div className="ms-auto">
+                <button
+                  className="btn btn-danger shadow"
+                  onClick={() => {
+                    dispatch(deleteTask(taskId));
+                    history.push(`/view/project/${projectId}`);
+                  }}
+                >
+                  Delete Task
+                </button>
+
+                <button
+                  className="btn btn-secondary shadow ms-3"
+                  onClick={() => {
+                    history.push(`/view/project/${projectId}`);
+                  }}
+                >
+                  Back
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="d-flex flex-column col-12 col-md-5 mt-4">
+            <div className="input-group mb-3 shadow">
+              <div className="input-group-prepend">
+                <label
+                  className="input-group-text"
+                  htmlFor="inputGroupSelect01"
+                >
+                  Assign Task to:
+                </label>
+              </div>
+              <select
+                className="form-select"
+                id="inputGroupSelect01"
+                onChange={(e) => setNewAssigned(e.target.value)}
+              >
+                <option defaultValue>
+                  {taskData.assigned ? taskData.assigned : "Unassigned"}
+                </option>
+                {taskData.assigned !== "Unassigned" && (
+                  <option>Unassigned</option>
+                )}
+                {users
+                  .filter((agent) => agent.name !== taskData.assigned)
+                  .map((agent) => (
+                    <option key={agent._id}>{agent.name}</option>
+                  ))}
+              </select>
+            </div>
+            <div className="input-group mb-3 shadow">
+              <div className="input-group-prepend">
+                <label
+                  className="input-group-text"
+                  htmlFor="inputGroupSelect01"
+                >
+                  Priority:
+                </label>
+              </div>
+              <select
+                className="form-select"
+                id="inputGroupSelect02"
+                onChange={(e) => setNewPriority(e.target.value)}
+              >
+                <option defaultValue>{taskData.priority}</option>
+                {taskData.priority !== "Low" && <option>Low</option>}
+                {taskData.priority !== "Medium" && <option>Medium</option>}
+                {taskData.priority !== "High" && <option>High</option>}
+              </select>
+            </div>
+            {taskData.attachedFiles.length ? (
+              <div className="col-12 mb-3">
+                <span>Attached files</span>
+                {taskData.attachedFiles.map((file) => (
+                  <div
+                    className="card d-flex flex-row p-1 mb-1 shadow"
+                    key={file.base64}
+                  >
+                    <span>{file.name}</span>
+                    <span className="ms-auto">
+                      <a download={file.name} href={file.base64}>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="18"
+                          height="18"
+                          fill="currentColor"
+                          className="bi bi-file-earmark-arrow-down"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M8.5 6.5a.5.5 0 0 0-1 0v3.793L6.354 9.146a.5.5 0 1 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 10.293V6.5z" />
+                          <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z" />
+                        </svg>
+                      </a>
+                    </span>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <div className="col-12 col-md-5 my-4">
+                <div className="card d-flex flex-row p-1 mb-1 shadow">
+                  <span>No files attached</span>
+                </div>
+              </div>
+            )}
+            <div className="d-flex flex-row flex-wrap justify-content-between">
+              <div className="col-12 col-lg-6">
+                <label htmlFor="formFileSm" className="form-label-sm">
+                  Invited Users
+                </label>
+                {taskData.allowedUsers.length &&
+                  taskData.allowedUsers.map((agent) => (
+                    <div
+                      key={agent}
+                      className="card d-flex flex-row p-2 mb-1 justify-content-between align-items-center shadow"
+                    >
+                      <span>{agent}</span>
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="18"
                         height="18"
                         fill="currentColor"
-                        className="bi bi-file-earmark-arrow-down"
+                        className="bi bi-person-x-fill text-danger"
                         viewBox="0 0 16 16"
+                        onClick={() => {
+                          removeAlloweduser(agent);
+                        }}
                       >
-                        <path d="M8.5 6.5a.5.5 0 0 0-1 0v3.793L6.354 9.146a.5.5 0 1 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 10.293V6.5z" />
-                        <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z" />
+                        <path
+                          fillRule="evenodd"
+                          d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm6.146-2.854a.5.5 0 0 1 .708 0L14 6.293l1.146-1.147a.5.5 0 0 1 .708.708L14.707 7l1.147 1.146a.5.5 0 0 1-.708.708L14 7.707l-1.146 1.147a.5.5 0 0 1-.708-.708L13.293 7l-1.147-1.146a.5.5 0 0 1 0-.708z"
+                        />
                       </svg>
-                    </a>
-                  </span>
-                </div>
-              ))}
-            </div>
-          ) : (
-            <div className="col-12 col-md-5 my-4">
-              <div className="card d-flex flex-row p-1 mb-1 shadow">
-                <span>No files attached</span>
+                    </div>
+                  ))}
               </div>
-            </div>
-          )}
-
-          <div className="mb-3 col-12 col-sm-12 col-md-6 col-lg-5 col-xl-5 col-xxl-4">
-            <div className="d-flex ">
-              <button
-                className="btn btn-secondary shadow"
-                onClick={() => {
-                  document.getElementById("taskForm").disabled = false;
-                  document
-                    .getElementById("saveButton")
-                    .classList.remove("disabled");
-                }}
-              >
-                Edit
-              </button>
-
-              <button
-                type="submit"
-                id="saveButton"
-                onClick={() => {
-                  handleSubmit();
-                }}
-                className="btn btn-primary ms-3 disabled shadow"
-              >
-                Save
-              </button>
-              <div className="input-group ms-3 shadow">
-                <label
-                  className="input-group-text"
-                  htmlFor="inputGroupSelect01"
-                >
-                  status
+              <div className="col-12 col-lg-5">
+                <label htmlFor="formFileSm" className="form-label-sm">
+                  Invite Users
                 </label>
-                <select
-                  className="form-select"
-                  id="inputGroupSelect01"
-                  onChange={(e) => setNewStatus(e)}
-                >
-                  <option value="default" defaultValue>
-                    {taskData.status}
-                  </option>
-                  {taskData.status !== "To do" ? (
-                    <option value="To do">To do</option>
-                  ) : null}
-                  {taskData.status !== "In progress" ? (
-                    <option value="In progress">In progress</option>
-                  ) : null}
-                  {taskData.status !== "Blocked" ? (
-                    <option value="Blocked">Blocked</option>
-                  ) : null}
-                  {taskData.status !== "Done" ? (
-                    <option value="Done">Done</option>
-                  ) : null}
-                </select>
+                <div className="input-group mb-1">
+                  <input
+                    type="text"
+                    className="form-control shadow"
+                    placeholder="Add user"
+                    aria-label="Add user"
+                    aria-describedby="basic-addon1"
+                    onChange={(e) => setSearchParameter(e)}
+                  />
+                </div>
+                <div className="mb-3">
+                  {searchUser !== "" &&
+                    users
+                      .filter(
+                        (agent) => !taskData.allowedUsers.includes(agent.name)
+                      )
+                      .filter((agent) =>
+                        agent.name.toLowerCase().includes(searchUser)
+                      )
+                      .map((agent) => (
+                        <div
+                          key={agent._id}
+                          className="card d-flex flex-row p-2 mb-1 justify-content-between align-items-center shadow"
+                        >
+                          <span>{agent.name}</span>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="18"
+                            height="18"
+                            fill="currentColor"
+                            className="bi bi-person-plus-fill text-success"
+                            viewBox="0 0 16 16"
+                            onClick={() => {
+                              addAlloweduser(agent.name);
+                            }}
+                          >
+                            <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                            <path
+                              fillRule="evenodd"
+                              d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"
+                            />
+                          </svg>
+                        </div>
+                      ))}
+                </div>
               </div>
             </div>
-          </div>
-          <div className="ms-auto mb-3">
-            <button
-              className="btn btn-danger shadow"
-              onClick={() => {
-                dispatch(deleteTask(taskId));
-                history.push(`/view/project/${projectId}`);
-              }}
-            >
-              Delete Task
-            </button>
-            <button
-              className="btn btn-secondary ms-3 shadow"
-              onClick={() => {
-                history.push(`/view/project/${projectId}`);
-              }}
-            >
-              Back
-            </button>
           </div>
         </div>
       )}
-      <div className="d-flex justify-content-between flex-wrap">
-        <div className="col-12 col-md-5">
-          <div className="input-group mb-3 shadow">
-            <div className="input-group-prepend">
-              <label className="input-group-text" htmlFor="inputGroupSelect01">
-                Assign Task to:
-              </label>
-            </div>
-            <select
-              className="form-select"
-              id="inputGroupSelect01"
-              onChange={(e) => setNewAssigned(e.target.value)}
-            >
-              <option defaultValue>
-                {taskData.assigned ? taskData.assigned : "Unassigned"}
-              </option>
-              {taskData.assigned !== "Unassigned" && (
-                <option>Unassigned</option>
-              )}
-              {users
-                .filter((agent) => agent.name !== taskData.assigned)
-                .map((agent) => (
-                  <option key={agent._id}>{agent.name}</option>
-                ))}
-            </select>
-          </div>
-        </div>
-        <div className="col-12 col-md-5">
-          <div className="input-group mb-3 shadow">
-            <div className="input-group-prepend">
-              <label className="input-group-text" htmlFor="inputGroupSelect01">
-                Priority:
-              </label>
-            </div>
-            <select
-              className="form-select"
-              id="inputGroupSelect02"
-              onChange={(e) => setNewPriority(e.target.value)}
-            >
-              <option defaultValue>{taskData.priority}</option>
-              {taskData.priority !== "Low" && <option>Low</option>}
-              {taskData.priority !== "Medium" && <option>Medium</option>}
-              {taskData.priority !== "High" && <option>High</option>}
-            </select>
-          </div>
-        </div>
-      </div>
-      <div className="d-flex justify-content-between flex-wrap">
-        <div className="col-md-5 col-12 col-md-5">
-          <h5>List of task viewers</h5>
-          {taskData.allowedUsers.length &&
-            taskData.allowedUsers.map((agent) => (
-              <div
-                key={agent}
-                className="card d-flex flex-row p-1 mb-1 justify-content-between align-items-center shadow"
-              >
-                <span>{agent}</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="18"
-                  height="18"
-                  fill="currentColor"
-                  className="bi bi-person-x-fill text-danger"
-                  viewBox="0 0 16 16"
-                  onClick={() => {
-                    removeAlloweduser(agent);
-                  }}
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm6.146-2.854a.5.5 0 0 1 .708 0L14 6.293l1.146-1.147a.5.5 0 0 1 .708.708L14.707 7l1.147 1.146a.5.5 0 0 1-.708.708L14 7.707l-1.146 1.147a.5.5 0 0 1-.708-.708L13.293 7l-1.147-1.146a.5.5 0 0 1 0-.708z"
-                  />
-                </svg>
-              </div>
-            ))}
-        </div>
-        <div className="d-flex flex-column col-12 col-md-5">
-          <h5>Search for users</h5>
-          <div className="input-group mb-1">
-            <input
-              type="text"
-              className="form-control shadow"
-              placeholder="Add user"
-              aria-label="Add user"
-              aria-describedby="basic-addon1"
-              onChange={(e) => setSearchParameter(e)}
-            />
-          </div>
-          <div className="mb-3">
-            {searchUser !== "" &&
-              users
-                .filter((agent) => !taskData.allowedUsers.includes(agent.name))
-                .filter((agent) =>
-                  agent.name.toLowerCase().includes(searchUser)
-                )
-                .map((agent) => (
-                  <div
-                    key={agent._id}
-                    className="card d-flex flex-row p-1 mb-1 justify-content-between align-items-center shadow"
-                  >
-                    <span>{agent.name}</span>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18"
-                      height="18"
-                      fill="currentColor"
-                      className="bi bi-person-plus-fill text-success"
-                      viewBox="0 0 16 16"
-                      onClick={() => {
-                        addAlloweduser(agent.name);
-                      }}
-                    >
-                      <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                      <path
-                        fillRule="evenodd"
-                        d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"
-                      />
-                    </svg>
-                  </div>
-                ))}
-          </div>
-        </div>
-      </div>
+
       {/* <SubTasks taskId={id} /> */}
     </div>
   );
